dd9237124be4824d89e8ef7ea5799b3ab2b47939
mblechn <69826523+mblechn@users.noreply.github.com>
2020-11-20 12:17:49 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { useMemo, useState } from 'react';
+import { Document, Page } from 'react-pdf';
 import './App.css';
+import samplePDF from './test.pdf';
+import { pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+function highlightPattern(text, patternsAsBarDelimitedString) {
+  var patternsAsArray = patternsAsBarDelimitedString.split('|');
+  var regexPattern = new RegExp(patternsAsArray[0] + "|" + patternsAsArray[1], 'gi')
+
+  const splitText = text.split(regexPattern);
+
+  if (splitText.length <= 1) {
+    return text;
+  }
+
+  const matches = text.match(regexPattern)
+
+  const result = splitText.reduce((arr, element, index) => (matches[index] ? [
+    ...arr,
+    element,
+    <mark key={index} className="mark">
+      {matches[index]}
+    </mark>
+  ] : [...arr, element]), []);
+
+  return result;
 }
 
-export default App;
+export default function App() {
+  const [searchText, setSearchText] = useState('');
+
+  // const textRenderer = useMemo((textItem) => {
+  //   console.log("textItem: " + textItem);
+  //   return;
+  //   //return highlightPattern(textItem.str, searchText);
+  // }, [searchText]);
+
+  function textRenderer(doc) {
+    return highlightPattern(doc.str, searchText);
+  }
+
+  function onChange(event) {
+    setSearchText(event.target.value);
+  }
+
+  return (
+    <>
+      <Document
+        file={samplePDF}
+      >
+        <Page
+          pageNumber={1}
+          customTextRenderer={textRenderer}
+        />
+      </Document>
+      <div>
+        <label htmlFor="search">Search:</label>
+        <input type="search" size="100" id="search" value={searchText} onChange={onChange} />
+      </div>
+    </>
+  );
+}
